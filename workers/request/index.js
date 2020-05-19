17bf191a53354454d5fb6617e146c94d0d16e158
ErLiangXiao <531015909@qq.com>
2020-05-19 22:27:02 +0800

    。。

--- workers/request/index.js
+++ workers/request/index.js
@@ -5,10 +5,10 @@ worker.onMessage((msg) => {
   console.log(msg);
   let msgList = msg.msgList // 聊天数据
   let value = msg.value // 查询条件
-  let lists = [] // 筛选完的列表
+  let list = [] // 筛选完的列表
   msgList.filter(element => {
     let arr = element.data
-    let count = 0 // 符合条件的条数
+    let count = 0 // 符合条件的条数 
     for (const item of arr) {
       if (item.type === 'emoji') {
 
